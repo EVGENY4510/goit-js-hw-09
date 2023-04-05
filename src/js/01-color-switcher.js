39ca1348d8d2324376e693fc0b83087837162944
EVGENY4510 <lentiay4510z@gmail.com>
2023-04-05 17:51:45 +0300

    hw-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+btnStartEl.disabled = false;
+btnStopEl.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+let timerId = null;
+btnStartEl.addEventListener('click', onBtnStart);
+
+function onBtnStart() {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  btnStartEl.disabled = true;
+  btnStopEl.disabled = false;
+}
+
+btnStopEl.addEventListener('click', onBtnStop);
+
+function onBtnStop() {
+  clearInterval(timerId);
+  btnStartEl.disabled = false;
+  btnStopEl.disabled = true;
+}
